6ec30a64304777efec6a3160ea57abcf473d52f0
Manan Gupta <guptamanan282@gmail.com>
2022-10-20 14:48:51 +0530

    calculated the distance and theta errors

--- scripts/controller.py
+++ scripts/controller.py
@@ -10,6 +10,8 @@ from nav_msgs.msg import Odometry
 # for finding sin() cos() 
 import math
 
+import numpy as np
+
 # Odometry is given as a quaternion, but for the controller we'll need to find the orientaion theta by converting to euler angle
 from tf.transformations import euler_from_quaternion
 
@@ -27,7 +29,7 @@ def odometryCb(msg):
 def main():
     # Initialze Node
     rospy.init_node("controller", anonymous=True)
-    # We'll leave this for you to figure out the syntax for 
+    # We'll leave this for you to figure out the syntax for
     # initialising node named "controller"
 	
     # Initialze Publisher and Subscriber
@@ -47,7 +49,10 @@ def main():
 
     # Initialise variables that may be needed for the control loop
     x_d,y_d,theta_d=2,2,0
-    Kp=2.10 
+    Kp=2.10
+	vel_x = 0
+	vel_y = 0
+	vel_z = 0
     # For ex: x_d, y_d, theta_d (in **meters** and **radians**) for defining desired goal-pose.
     # and also Kp values for the P Controller
 
@@ -56,12 +61,20 @@ def main():
     # Control Loop goes here
     #
     #
-    while not rospy.is_shutdown():
+	while not rospy.is_shutdown():
 
 		# Find error (in x, y and theta) in global frame
 		# the /odom topic is giving pose of the robot in global frame
 		# the desired pose is declared above and defined by you in global frame
 		# therefore calculate error in global frame
+		dist_error = math.sqrt(math.pow((hola_x - x_d),2) + math.pow((hola_y - y_d),2))
+		theta_goal = np.arctan((y_d -hola_y)/(x_d - hola_x))   #slope
+        if theta_goal>0:
+            theta_goal+=0.04
+        elif theta_goal<0:
+            theta_goal-=0.04
+        bot_theta=hola_theta  
+        theta_error = round(bot_theta - theta_goal, 2)
 
 		# (Calculate error in body frame)
 		# But for Controller outputs robot velocity in robot_body frame, 
@@ -83,7 +96,7 @@ def main():
         vel.linear.y = vel_y
         vel.angular.z = vel_z
 
-        pub.publish(vel)
+		pub.publish(vel)
         rate.sleep()
 
 if __name__ == "__main__":
