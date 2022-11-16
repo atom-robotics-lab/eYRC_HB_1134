a797efaf273be9d73c3de8308b123f61890f5908
Kartik9250 <kartikrana9250@gmail.com>
2022-11-16 22:24:08 +0530

    fixed the velocities

--- Task01/scripts/controller.py
+++ Task01/scripts/controller.py
@@ -20,8 +20,8 @@ from geometry_msgs.msg import PoseArray
 hola_x = 0
 hola_y = 0
 hola_theta = 0
-x_goals, y_goals, theta_goals = [
-    1, -1, -1, 1, 0], [1, 1, -1, -1, 0], [0.785, 2.335, -2.335, -0.785, 0]
+x_goals, y_goals, theta_goals = [],[],[]
+#x_goals, y_goals, theta_goals = [1, -1, -1, 1, 0], [1, 1, -1, -1, 0], [0.785, 2.335, -2.335, -0.785, 0]
 
 # The callback function for the position of the bot
 
@@ -34,22 +34,19 @@ def odometryCb(msg):
         [msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])[2]
 
 
-# def task1_goals_Cb(msg):
-#     global x_goals, y_goals, theta_goals
-
-#     x_goals.clear()
-#     y_goals.clear()
-#     theta_goals.clear()
-
-#     for waypoint_pose in msg.poses:
-#         x_goals.append(waypoint_pose.position.x)
-#         y_goals.append(waypoint_pose.position.y)
-
-#         orientation_q = waypoint_pose.orientation
-#         orientation_list = [orientation_q.x,
-#                             orientation_q.y, orientation_q.z, orientation_q.w]
-#         theta_goal = euler_from_quaternion(orientation_list)[2]
-#         theta_goals.append(theta_goal)
+def task1_goals_Cb(msg):
+    global x_goals, y_goals, theta_goals
+    x_goals.clear()
+    y_goals.clear()
+    theta_goals.clear()
+    for waypoint_pose in msg.poses:
+        x_goals.append(waypoint_pose.position.x)
+        y_goals.append(waypoint_pose.position.y)
+        orientation_q = waypoint_pose.orientation
+        orientation_list = [orientation_q.x,
+                            orientation_q.y, orientation_q.z, orientation_q.w]
+        theta_goal = euler_from_quaternion(orientation_list)[2]
+        theta_goals.append(theta_goal)
 
 
 def main():
@@ -59,110 +56,61 @@ def main():
     # Initialzing Publisher and Subscriber for cmd_vel and odometry
     pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
     rospy.Subscriber('/odom', Odometry, odometryCb)
-    # rospy.Subscriber('task1_goals', PoseArray, task1_goals_Cb)
+    rospy.Subscriber('task1_goals', PoseArray, task1_goals_Cb)
 
     # Declare a Twist message for the velocity
     vel = Twist()
 
-    # rospy.wait_for_message("/task1_goals", PoseArray)
+    rospy.wait_for_message("/task1_goals", PoseArray)
 
     # For maintaining control loop rate.
     rate = rospy.Rate(100)
 
-    # defining the goal vaariables
+    # defining the goal variables
     x_d, y_d, theta_d = x_goals[0], y_goals[0], theta_goals[0]
-    # x_d, y_d, theta_d = -1, -1, 1
+    #x_d, y_d, theta_d = 0, -2, 0
 
     # initializing variable kp for the P-controller
-    kp = 0.40
+    kp = 1.1
+    ka = 2
+    count = 0
 
     # Control Loop
     while not rospy.is_shutdown():
         print("odom: " + str(hola_x)+" "+str(hola_y)+" "+str(hola_theta))
+        dist_error = math.sqrt(math.pow((x_d - hola_x), 2) + math.pow((y_d - hola_y), 2))
+
+        dist_tolerance = 0.05
+        theta_tolerance = 0.01745
 
         # Find error (in x, y and theta) in global frame
-        dist_error = math.sqrt(
-            math.pow((x_d - hola_x), 2) + math.pow((y_d - hola_y), 2))
-        theta_error = round(theta_d - hola_theta, 2)
+        e_g_theta = theta_d - hola_theta
+        e_g_x = x_d - hola_x
+        e_g_y = y_d - hola_y
 
-        x_error = round(x_d-hola_x, 2)
-        y_error = round(y_d-hola_y, 2)
+        e_b_theta = e_g_theta
+        e_b_x = math.cos(hola_theta)*e_g_x + math.sin(hola_theta)*e_g_y
+        e_b_y = -math.sin(hola_theta)*e_g_x + math.cos(hola_theta)*e_g_y
 
-        x = x_error+x_error*math.cos(theta_error)
-        y = y_error+y_error*math.sin(theta_error)
-
-        theta_tolerance = 0.01745
-        dist_tolerance = 0.05
-
-        # defining the variables for velocity
-        vel_x = 0
-        vel_y = 0
-        vel_z = 0
-        count = 0
-
-        rospy.loginfo("theta error: %f, distance error: %f",
-                      theta_error, dist_error)
-
-        # comparing the errors with their tolerance and moving the bot accordingly
-        if -theta_tolerance < theta_error < +theta_tolerance:
-            if -dist_tolerance < dist_error < +dist_tolerance:
-                # both the errors are in their tolerance values hence the goal has been reached
-                vel_x = 0
-                vel_y = 0
-                print("goal reached")
-
-                if count < len(x_goals):
-                    count += 1
-                    try:
-                        x_d = x_goals[count]
-                        y_d = y_goals[count]
-                        theta_d = theta_goals[count]
-                    except:
-                        print("Run Completed")
-
-            elif abs(dist_error) > dist_tolerance:
-                print("if")
-                while abs(dist_error) > dist_tolerance:
-                    print("while")
-                    # There is error in distance so moving the bot in straight line
-                    vel.linear.x = x*kp
-                    vel.linear.y = y*kp
-                    pub.publish(vel)
-                    dist_error = math.sqrt(
-                        math.pow((x_d - hola_x), 2) + math.pow((y_d - hola_y), 2))
-                    x_error = round(x_d-hola_x, 2)
-                    y_error = round(y_d-hola_y, 2)
-
-                    x = x_error+x_error*math.cos(theta_error)
-                    y = y_error+y_error*math.sin(theta_error)
-
-        # There is theta error so rotating the bot along its axis
-        elif theta_error > +theta_tolerance:
-            vel_z = theta_error*kp*3
-        # There is theta error so rotating the bot along its axis
-        elif theta_error < -theta_tolerance:
-            vel_z = -theta_error*kp*3
-
-        # if vel_x > 0.55:
-        #     vel_x = 0.55
-
-        # elif vel_x < -0.55:
-        #     vel_x = -0.55
-
-        # if vel_y > 0.55:
-        #     vel_y = 0.55
-
-        # elif vel_y < -0.55:
-        #     vel_y = -0.55
-
-        vel.linear.x = vel_x
-        vel.linear.y = vel_y
-        vel.angular.z = vel_z
+        vel.linear.x = e_b_x*kp
+        vel.linear.y = e_b_y*kp
+        vel.angular.z = e_b_theta*ka
 
         # publishing the velocity
         pub.publish(vel)
         rate.sleep()
 
+        if abs(dist_error) < dist_tolerance:
+            if abs(e_b_theta) < theta_tolerance:
+                try:
+                    vel.linear.x = 0
+                    vel.linear.y = 0
+                    vel.angular.z = 0
+                    pub.publish(vel)
+                    x_d,y_d,theta_d = x_goals[count],y_goals[count],theta_goals[count]
+                    count +=1 
+                except:
+                    print("goal reached")
 
 if __name__ == "__main__":
     try:
